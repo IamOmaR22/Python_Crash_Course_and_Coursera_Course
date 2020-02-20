cebf05680d725fb5e4ae15d6943a6f9b00e9cadf
Md. Omar Faruk <iamomar022@gmail.com>
2020-02-20 12:21:01 +0600

    Introdicing Lists

--- chapter3.py
+++ chapter3.py
@@ -1,47 +1,103 @@
 # Chapter 3 _ Introducing Lists
 
+## Length Of List Start ##
+print("-- Length Using Loop --")
 bus=['hanif','shyamoly','desh travels','BRTC',"2423"]
 
-# for i in range(0,len(bus)):
-#     print(i)
+for i in range(0,len(bus)):
+    print(i)
+## Length Of List End ##
 
-## Adding Elements to a List (Appending Elements to the End of a List)
-# bus.append('GRAMEEN')
-# print(bus)
+print("")
+print("")
 
-## Insert Elements into a List (Any Position)
-# bus.insert(2,'green line')
-# print(bus)
+## Adding Elements to a List (Appending Elements to the End of a List) Start ##
+print("-- Append --")
+name = ['omar','lutfa','faruk','biki','kaium','abir','shafin']
+name.append('riajul')
+print(name)
+## Adding Elements to a List (Appending Elements to the End of a List) End ##
 
-## Removing Elements from a List (Removing an Item by Value)
-# bus.remove("hanif")
-# print(bus)
+print("")
+print("")
 
-## Removing an Item Using the pop() Method (Popping Items from any Position in a List)
-# mehedi=bus.pop(1)
-# print(mehedi)
-# print(bus)
+## Insert Elements into a List (Any Position) Start ##
+print("-- Insert --")
+name2 = ['omar','lutfa','faruk','biki']
+name2.insert(2,'arafat ullah nur')
+print(name2)
+## Insert Elements into a List (Any Position) End ##
 
-## Removing an Item Using the del Statement
-# del bus[0]
-# print(bus)
+print("")
+print("")
 
-## Sorting a List Permanently with the sort() Method
-# bus.sort()
-# print(bus)
+## Removing Elements from a List (Removing an Item by Value) Start ##
+print("-- Removing Elements --")
+bus=['hanif','shyamoly','desh travels','BRTC',"2423"]
+bus.remove("hanif")
+print(bus)
+## Removing Elements from a List (Removing an Item by Value) End ##
 
-## Sorting a List Temporarily with the sorted() Function
-# lis=[]
-# lis=sorted(bus)
-# print(lis)
-# print(bus)
+print("")
+print("")
 
-## Printing a List in Reverse Order
-# bus.reverse()
-# print(bus)
+## Removing an Item Using the pop() Method (Popping Items from any Position in a List) Start ##
+print("-- Removing an Item Using pop() --")
+bus=['hanif','shyamoly','desh travels','BRTC','mtcl',"2423"]
+mehedi=bus.pop(1)
+print(mehedi)
+print(bus)
+## Removing an Item Using the pop() Method (Popping Items from any Position in a List) End ##
 
-## Finding the Length of a List
-# l=len(bus)
-# print(bus)
-# print(l)
+print("")
+print("")
 
+## Removing an Item Using the del Statement Start ##
+print("-- Removing an Item Using del --")
+bus=['hanif','shyamoly','desh travels','BRTC',"2423"]
+del bus[0]
+print(bus)
+## Removing an Item Using the del Statement End ##
+
+print("")
+print("")
+
+## Sorting a List Permanently with the sort() Method Start ##
+print("-- Sorting Permanently Using sort() --")
+bus=['hanif','shyamoly','desh travels','BRTC',"2423"]
+bus.sort()
+print(bus)
+## Sorting a List Permanently with the sort() Method End ##
+
+print("")
+print("")
+
+## Sorting a List Temporarily with the sorted() Function Start ##
+print("-- Sorting Temporarily Using sorted() --")
+bus=['hanif','shyamoly','desh travels','BRTC',"2423"]
+lis=[]
+lis=sorted(bus)
+print(lis)
+print(bus)
+## Sorting a List Temporarily with the sorted() Function End ##
+
+print("")
+print("")
+
+## Printing a List in Reverse Order Start ##
+print("-- Reverse Order --")
+bus=['hanif','shyamoly','desh travels','BRTC',"2423"]
+bus.reverse()
+print(bus)
+## Printing a List in Reverse Order End ##
+
+print("")
+print("")
+
+## Finding the Length of a List Start ##
+print("-- Length of List --")
+bus=['hanif','shyamoly','desh travels','BRTC',"2423"]
+l=len(bus)
+print(bus)
+print(l)
+## Finding the Length of a List End ##
