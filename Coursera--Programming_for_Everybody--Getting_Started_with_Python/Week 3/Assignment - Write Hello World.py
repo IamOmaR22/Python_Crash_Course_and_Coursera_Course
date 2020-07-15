ddfffbb8df34601f5c1a79352ec5ebd60e0ed2b0
Md. Omar Faruk <iamomar022@gmail.com>
2020-07-15 11:15:36 +0600

    Week 3 4 and 5 Done

---
+++ Coursera--Programming_for_Everybody--Getting_Started_with_Python/Week 3/Assignment - Write Hello World.py
@@ -0,0 +1,22 @@
+'''
+Write a program that uses a print statement to say 'hello world' as shown in
+'Desired Output'.
+'''
+# The code below almost works
+
+
+--------------
+
+
+print("hello world")
+
+
+--------------
+
+'''
+Your Output
+hello world
+
+Desired Output
+hello world
+'''
